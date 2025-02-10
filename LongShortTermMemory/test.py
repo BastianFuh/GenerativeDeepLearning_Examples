c1f96527cd8c6e80641607303849f267072fdec7
Baeshen <bastian.fuhlenriede@gmail.com>
2025-02-10 21:11:26 +0100

    Implemented first version of the test script for the lstm

--- LongShortTermMemory/test.py
+++ LongShortTermMemory/test.py
@@ -1,44 +1,69 @@
 import torch
-from model import DCGAN
+from torch.nn import functional as F
+from model import LSTM
+
 import numpy as np
+from tokenizers import Tokenizer, Encoding
 import os
 from matplotlib import pyplot as plt
 
 
-def preprocess(imgs):
-    imgs = np.array(imgs["image"]).astype("float32") / 255.0
-    imgs = np.pad(imgs, ((2, 2), (2, 2)), constant_values=0.0)
-
-    return {"data": imgs}
-
-
-def collate_fn(batch):
-    result = dict()
-
-    result["label"] = torch.tensor([x["label"] for x in batch], dtype=torch.float32)
-    result["data"] = torch.tensor([[x["data"]] for x in batch], dtype=torch.float32)
-
-    return result
-
-
-EMBEDDING_SIZE = 2
-USE_MULTI_VARIATIONAL = True
-
-
-MODEL_PATH = "gan.pt"
+MODEL_PATH = "lstm.pt"
+TOKENIZER_PATH = "tokenizer_lstm.pt"
 
 DEVICE = "cuda"
 
+
+class TextGenerator:
+    def __init__(self, top_k=10):
+        self.top_k = top_k
+
+        self.model = LSTM()
+        if not os.path.exists(MODEL_PATH):
+            print("Pleasure ensure that a model exists")
+            exit()
+
+        self.model.load_state_dict(torch.load(MODEL_PATH, weights_only=True))
+        self.model.eval()
+        self.model = self.model.to(DEVICE)
+
+        self.tokenizer: Tokenizer = Tokenizer.from_file(TOKENIZER_PATH)
+
+        self.random_generator = np.random.Generator(np.random.PCG64())
+
+    def sample_from(self, probs, temperature):
+        probs = probs  # ** (1 / temperature)
+        probs = probs  # / torch.sum(probs, dim=-1)
+        probs = probs.cpu().detach().numpy().flatten()
+        return self.random_generator.choice(len(probs), p=probs), probs
+
+    def generate(self, start_prompt, max_tokens, temperature):
+        prompt = start_prompt
+        sample_token = None
+
+        iteration = 0
+        while iteration < max_tokens and sample_token != 0:
+            iteration += 1
+            input_tokens: Encoding = self.tokenizer.encode(prompt)
+
+            x = torch.tensor([input_tokens.ids], dtype=torch.long).to(DEVICE)
+            input_length = len(input_tokens)
+            y = F.softmax(self.model(x, input_length), dim=-1)
+            print(y.shape)
+
+            out = self.sample_from(y, temperature)
+
+            prompt += f" {self.tokenizer.decode([out[0]])}"
+
+            print(f"{iteration}: {prompt}")
+
+
 if __name__ == "__main__":
-    model = DCGAN()
+    generator = TextGenerator()
 
-    if not os.path.exists(MODEL_PATH):
-        print("Pleasure ensure that a model exists")
-        exit()
+    generator.generate("recipe for", 50, 1.0)
 
-    model.load_state_dict(torch.load(MODEL_PATH, weights_only=True))
-    model.eval()
-    model = model.to(DEVICE)
+    exit()
 
     # Shows a grid of processed test images
     data = torch.randn(size=(16, 100, 1, 1), device=DEVICE)
