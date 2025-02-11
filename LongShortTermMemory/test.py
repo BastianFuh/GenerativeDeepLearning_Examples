0828b9da8f74380eb294ddd8084644d852df09a5
Baeshen <bastian.fuhlenriede@gmail.com>
2025-02-11 12:45:48 +0100

    Updated test script

--- LongShortTermMemory/test.py
+++ LongShortTermMemory/test.py
@@ -32,8 +32,8 @@ class TextGenerator:
         self.random_generator = np.random.Generator(np.random.PCG64())
 
     def sample_from(self, probs, temperature):
-        probs = probs  # ** (1 / temperature)
-        probs = probs  # / torch.sum(probs, dim=-1)
+        probs = probs ** (1 / temperature)
+        probs = probs / torch.sum(probs, dim=-1)
         probs = probs.cpu().detach().numpy().flatten()
         return self.random_generator.choice(len(probs), p=probs), probs
 
@@ -47,9 +47,8 @@ class TextGenerator:
             input_tokens: Encoding = self.tokenizer.encode(prompt)
 
             x = torch.tensor([input_tokens.ids], dtype=torch.long).to(DEVICE)
-            input_length = len(input_tokens)
-            y = F.softmax(self.model(x, input_length), dim=-1)
-            print(y.shape)
+
+            y = self.model(x)
 
             out = self.sample_from(y, temperature)
 
@@ -57,26 +56,10 @@ class TextGenerator:
 
             print(f"{iteration}: {prompt}")
 
+        print("\n", prompt)
+
 
 if __name__ == "__main__":
     generator = TextGenerator()
 
-    generator.generate("recipe for", 50, 1.0)
-
-    exit()
-
-    # Shows a grid of processed test images
-    data = torch.randn(size=(16, 100, 1, 1), device=DEVICE)
-
-    data = data.to(DEVICE)
-
-    output = model.generator(data)
-    fig = plt.figure()
-    ax = fig.subplots(2, 8)
-
-    for i in range(0, 16):
-        row = int(i / 8)
-        column = int(i % 8)
-        ax[row, column].imshow(output.cpu().detach().numpy()[i].squeeze(), cmap="gray")
-
-    plt.show()
+    generator.generate("recipe for a vegetarian", 201, 0.5)
