81a34d1e193256d3d9724f3f6a793ed71669daf3
Baeshen <bastian.fuhlenriede@gmail.com>
2025-02-17 14:42:06 +0100

    Implemented inference for diffusion

--- Diffusion/test.py
+++ Diffusion/test.py
@@ -1,66 +1,39 @@
 import torch
-from torch.nn import functional as F
-from model import LSTM
 
-import numpy as np
-from tokenizers import Tokenizer, Encoding
 import os
+
 from matplotlib import pyplot as plt
 
+from model import DiffusionModel
 
-MODEL_PATH = "lstm.pt"
-TOKENIZER_PATH = "tokenizer_lstm.pt"
+
+MODEL_PATH = "diffusion.pt"
 
 DEVICE = "cuda"
 
 
-class TextGenerator:
-    def __init__(self, top_k=10):
-        self.top_k = top_k
-
-        self.model = LSTM()
-        if not os.path.exists(MODEL_PATH):
-            print("Pleasure ensure that a model exists")
-            exit()
-
-        self.model.load_state_dict(torch.load(MODEL_PATH, weights_only=True))
-        self.model.eval()
-        self.model = self.model.to(DEVICE)
-
-        self.tokenizer: Tokenizer = Tokenizer.from_file(TOKENIZER_PATH)
-
-        self.random_generator = np.random.Generator(np.random.PCG64())
-
-    def sample_from(self, probs, temperature):
-        probs = probs ** (1 / temperature)
-        probs = probs / torch.sum(probs, dim=-1)
-        probs = probs.cpu().detach().numpy().flatten()
-        return self.random_generator.choice(len(probs), p=probs), probs
-
-    def generate(self, start_prompt, max_tokens, temperature, print_intermidiate=False):
-        prompt = start_prompt
-        sample_token = None
-
-        iteration = 0
-        while iteration < max_tokens and sample_token != 0:
-            iteration += 1
-            input_tokens: Encoding = self.tokenizer.encode(prompt)
-
-            x = torch.tensor([input_tokens.ids], dtype=torch.long).to(DEVICE)
-
-            y = self.model(x)
-
-            out = self.sample_from(y, temperature)
-
-            prompt += f" {self.tokenizer.decode([out[0]])}"
-
-            if print_intermidiate:
-                print(f"{iteration}: {prompt}")
-
-        print("\n", prompt)
-
-
 if __name__ == "__main__":
-    generator = TextGenerator()
+    model = DiffusionModel(use_ema=False)
+    if os.path.exists(MODEL_PATH):
+        print("Loaded model")
+        model.load_state_dict(torch.load(MODEL_PATH, weights_only=True), strict=False)
+        model.create_ema()
+        model.ema_network.load_state_dict(
+            torch.load(f"ema-{MODEL_PATH}", weights_only=True)
+        )
 
-    generator.generate("recipe for vegetarian", 201, 0.5)
+    model.eval()
+
+    num_images = 10
+    steps = 20
+
+    img, initial_noise = model.generate(num_images, steps)
+
+    fig = plt.figure()
+    ax = fig.subplots(1, num_images)
+
+    for index, img in enumerate(img):
+        ax[index].imshow(img.transpose(0, 2).detach().numpy())
+        ax[index].axis("off")
+
+    plt.show()
